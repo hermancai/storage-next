6e73898ed0f0b62577f58d123e53c53f8d87a20e
Herman Cai <hermancai3@gmail.com>
2023-10-24 18:43:21 -0700

    Use supabase trigger to create root folder

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -10,19 +10,6 @@ export async function GET(req: Request) {
     if (code) {
         const supabase = createRouteHandlerClient({ cookies });
         await supabase.auth.exchangeCodeForSession(code);
-
-        // create root folder for new user
-        const {
-            data: { session },
-        } = await supabase.auth.getSession();
-        if (session !== null) {
-            const createRootRes = await supabase
-                .from("folder")
-                .insert({ name: "Home", parent: null });
-            if (createRootRes.error) {
-                console.log(createRootRes.error);
-            }
-        }
     }
 
     // take user back to landing page
--- supabase.sql
+++ supabase.sql
@@ -103,3 +103,16 @@ BEGIN
   RETURN '{"data":true}';
 END;
 $$
+
+
+----- Function/Trigger: create_root_folder -----
+create or replace function create_root_folder() returns trigger as $$ 
+begin
+  insert into public.folder (name, user_id)
+  values('Home', new.id);
+  return new;
+end;
+$$ language plpgsql security definer;
+create trigger on_auth_user_created
+  after insert on auth.users
+  for each row execute procedure create_root_folder();
