910727e58dc89e4c6a8582a5937402d10481d8ab
Herman Cai <hermancai3@gmail.com>
2025-10-16 04:31:07 -0700

    Edit about text

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,11 +7,8 @@ export default function AboutPage() {
             <div className="flex flex-col max-w-lg gap-4 text-zinc-100">
                 <h1 className="text-3xl">Thanks for visiting!</h1>
                 <p>
-                    This site was built for learning and demonstrative purposes.
-                    This means that while account creation and uploading are
-                    free and functional, other features are merely visual or in
-                    progress. This site is also subject to shutting down
-                    anytime.
+                    This is a test site I made for learning Next.js, PostgreSQL,
+                    and AWS.
                 </p>
                 <Link
                     href="/"
